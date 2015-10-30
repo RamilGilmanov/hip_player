58c0c7fb6118fb0d725dc3595c75c5f941203ec2
Ramil <ramil.gilmanov@flatstack.com>
2015-10-30 18:44:21 +0300

    Force russian speaker

--- app/services/speaker.rb
+++ app/services/speaker.rb
@@ -9,13 +9,7 @@ class Speaker
   end
 
   def call
-    speech = Speech.new(@text, voice: language_code)
+    speech = Speech.new(@text, voice: "ru")
     speech.speak
-  end
-
-  private
-
-  def language_code
-    @text.language.to_s[0..1] || "en"
   end
 end
